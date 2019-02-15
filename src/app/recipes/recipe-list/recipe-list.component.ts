2c75112e0b3e83283c9c4472f0187eb0f1815031
nicolasvahman <kavehv@live.dk>
2019-02-15 15:12:03 +0100

    Boks med billede og tekst

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe} from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('Test', 'Dette er en test', 'https://tennisshoppen.dk/images/WILSON%20PRO%20STAFF%2025%20JUNIOR%20TENNISKETCHER%202.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
