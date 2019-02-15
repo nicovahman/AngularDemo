2dbc44748d7aa150ba19f5dcc0d1616a15a83008
nicolasvahman <kavehv@live.dk>
2019-02-15 14:29:59 +0100

    Header osv

---
+++ src/app/recipes/recipe-detail/recipe-detal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-detal',
+  templateUrl: './recipe-detal.component.html',
+  styleUrls: ['./recipe-detal.component.css']
+})
+export class RecipeDetalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
